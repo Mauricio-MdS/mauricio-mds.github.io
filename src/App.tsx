d44b28eebe687b5bc7dfd9ae1f09c1bf92b0df54
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-06-11 11:38:44 -0300

    apply css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import Hero from './components/Hero/Hero';
-import Contact from './components/Contact/Contact';
-import Portfolio from './components/Portfolio/Portfolio';
+import React from "react";
+import { Stack } from "@mui/material";
+import Hero from "./components/Hero/Hero";
+import Contact from "./components/Contact/Contact";
+import Portfolio from "./components/Portfolio/Portfolio";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
-    <Stack spacing={2}>
-      <Hero/>
-      <Portfolio/>
-      <Contact/>
-    </Stack>
-    
+    <CssBaseline>
+      <Stack spacing={2}>
+        <Hero />
+        <Portfolio />
+        <Contact />
+      </Stack>
+    </CssBaseline>
   );
 }
 
