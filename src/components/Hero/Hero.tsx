bfa030dafd45e54bfe0bdbd426e271980b2869dc
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-06-11 17:20:26 -0300

    update hero design and description

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,24 +1,46 @@
-import heroImg from "./hero.jpeg";
 import React from "react";
-import { Avatar, Stack, Typography } from "@mui/material";
+import { Avatar, Card, Grid, Stack, Typography } from "@mui/material";
+import hero200 from "./hero200.jpeg";
+import hero300 from "./hero300.jpeg";
+import hero400 from "./hero400.jpeg";
+import hero500 from "./hero500.jpeg";
+import hero600 from "./hero600.jpeg";
 
 function Hero() {
   return (
-    <Stack
-      spacing={3}
-      component="header"
-      bgcolor="primary.main"
-      textAlign="center"
-      alignItems="center"
-      p="16px"
-    >
-      <Typography variant="h1" content="h1" gutterBottom>
-        Full Stack Developer
-      </Typography>
-      <Typography variant="h4" content="body1" align="center">
-        Hello, I am Mauricio and I build web applications
-      </Typography>
-      <Avatar src={heroImg} sx={{ height: "160px", width: "160px" }} />
+    <Stack bgcolor="primary.main" p="32px" alignItems="center" component="header">
+      <Card sx={{ maxWidth: 1200 }} component='article' raised>
+        <Grid container alignItems="center" textAlign="center" spacing={2}>
+          <Grid item xs={12} sm={6} md={3} component='figure'>
+            <Avatar
+              alt=""
+              variant="rounded"
+              src={hero200}
+              srcSet={`
+              ${hero200} 200w, 
+              ${hero300} 300w, 
+              ${hero400} 400w, 
+              ${hero500} 500w, 
+              ${hero600} 600w`}
+              sx={{ height: "auto", width: "100%" }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={9} p={1}>
+            <Typography variant="h2" content="h1" gutterBottom>
+              Full Stack Developer
+            </Typography>
+            <Typography variant="h5" content="body1">
+              Welcome! I'm Mauricio, a web developer specializing in building
+              dynamic and user-friendly web applications. My skillset includes
+              Javascript, Typescript, React, Node, and Java. Whether it's
+              creating intuitive front-end interfaces or designing efficient
+              back-end systems, I'm dedicated to bringing ideas to life through
+              code. Explore my portfolio to see some of my recent projects and
+              feel free to get in touch.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Card>
     </Stack>
   );
 }
