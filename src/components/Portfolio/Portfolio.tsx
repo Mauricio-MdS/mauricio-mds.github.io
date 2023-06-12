3657a7ff0440cfb20813990b47d6f09edba47ee8
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-06-12 20:42:43 -0300

    update portfolio text and image

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -9,7 +9,7 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import porfolioImg from './portfolio.png';
+import githubImg from "./github.png";
 
 function Portfolio() {
   return (
@@ -25,18 +25,26 @@ function Portfolio() {
       </Typography>
       <Card component="article">
         <CardContent>
-          <CardMedia sx={{ height: 140 }} image={porfolioImg} title="" />
+          <CardMedia sx={{ height: 140 }} image={githubImg} title="" />
           <Typography gutterBottom variant="h5" component="div">
             Personal site
           </Typography>
-          <Stack direction='row' justifyContent='center' spacing={1} marginBottom='8px'>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            spacing={1}
+            marginBottom="8px"
+          >
             <Chip label="React" color="info" size="small" />
             <Chip label="Typescript" color="info" size="small" />
             <Chip label="Material UI" color="info" size="small" />
+            <Chip label="Github" color="info" size="small" />
           </Stack>
 
           <Typography variant="body2" color="text.secondary">
-            You can check this site source code.
+            Check the source code of this page, which was developed using React
+            and TypeScript. The design incorporates Material UI components.
+            Deployed on GitHub Pages.
           </Typography>
         </CardContent>
         <CardActions>
