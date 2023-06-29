cf21ae43fe43e0d6467fe73ccf729d64ecc49c8f
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-06-29 16:36:54 -0300

    update projects with message board

--- src/components/Portfolio/projects.ts
+++ src/components/Portfolio/projects.ts
@@ -1,10 +1,25 @@
 import { PortfolioType } from "./PortfolioType";
 import portfolioImg from "./assets/portfolio.avif";
+import messageBoardImg from "./assets/messageboard.avif"
 
 export const projects: PortfolioType[] = [
   {
-    description:
-      "Check the source code of this page, which was developed using React and TypeScript. The design incorporates Material UI components. Deployed on GitHub Pages.",
+    description: "A message board. Please, leave your message.",
+    image: messageBoardImg,
+    liveLink: "https://msgboard.fly.dev/",
+    sourceLink: "https://github.com/Mauricio-MdS/message-board",
+    technologies: [
+      "MongoDB",
+      "Express.js",
+      "Node.js",
+      "Javascript",
+      "EJS",
+      "Docker",
+    ],
+    title: "Message Board",
+  },
+  {
+    description: "Check the source code of this page.",
     image: portfolioImg,
     sourceLink: "https://github.com/mauricio-mds",
     technologies: ["React", "Typescript", "Material UI", "Github"],
