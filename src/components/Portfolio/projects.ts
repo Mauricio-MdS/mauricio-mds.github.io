c6d6cfc913935ae1aa441846107e02556f61a3c2
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-07-18 14:22:19 -0300

    add crud api project

--- src/components/Portfolio/projects.ts
+++ src/components/Portfolio/projects.ts
@@ -1,8 +1,24 @@
 import { PortfolioType } from "./PortfolioType";
 import portfolioImg from "./assets/portfolio.avif";
-import messageBoardImg from "./assets/messageboard.avif"
+import messageBoardImg from "./assets/messageboard.avif";
+import crudApiImg from "./assets/crudapi.avif";
 
 export const projects: PortfolioType[] = [
+  {
+    description:
+      "A CRUD API with authentication and authorization capabilities that allows you to perform Create, Read, Update, and Delete operations on posts.",
+    image: crudApiImg,
+    sourceLink: "https://github.com/Mauricio-MdS/crud-api",
+    technologies: [
+      "MongoDB",
+      "Express.js",
+      "Node.js",
+      "Typescript",
+      "JsonWebToken",
+      "Bcrypt",
+    ],
+    title: "Express CRUD API",
+  },
   {
     description: "A message board. Please, leave your message.",
     image: messageBoardImg,
