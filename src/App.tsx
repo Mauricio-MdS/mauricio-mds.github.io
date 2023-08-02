3c53343fabdaefc615a608f731cee120d65d0278
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-08-02 15:32:21 -0300

    redirect to newer portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { Stack } from "@mui/material";
-import Hero from "./components/Hero/Hero";
-import Portfolio from "./components/Portfolio/Portfolio";
-import CssBaseline from "@mui/material/CssBaseline";
+import React from "react"
+import { Stack } from "@mui/material"
+import Hero from "./components/Hero/Hero"
+import Portfolio from "./components/Portfolio/Portfolio"
+import CssBaseline from "@mui/material/CssBaseline"
 
 function App() {
   return (
-    <CssBaseline>
-      <Stack spacing={2}>
-        <Hero />
-        <Portfolio />
-      </Stack>
-    </CssBaseline>
-  );
+    <p>
+      Redirecting. If you are not redirected, go to{" "}
+      <a href="https://mauriciosilva.vercel.app/">
+        https://mauriciosilva.vercel.app/
+      </a>
+    </p>
+    // <CssBaseline>
+    //   <Stack spacing={2}>
+    //     <Hero />
+    //     <Portfolio />
+    //   </Stack>
+    // </CssBaseline>
+  )
 }
 
-export default App;
+export default App
